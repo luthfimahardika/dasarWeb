ed1dcca5071efdf177c59cebd2136d1becf354b3
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2024-09-25 14:00:26 +0700

    js5-praktikum2

--- JOBSHEET5/array_2.php
+++ JOBSHEET5/array_2.php
@@ -3,7 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
+        <title>Tampilan Data Dosen</title>
+        <style>
+            table {
+                width: 50%;
+                margin: 20px auto;
+                border-collapse: collapse;
+                font-family: Arial, sans-serif;
+            }
+            table, th, td {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
+            th {
+                background-color: #f2f2f2;
+                text-align: left;
+            }
+            tr:hover {
+                background-color: #f5f5f5;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -12,11 +31,25 @@
             'domisili' => 'Malang',
             'jenis_kelamin' => 'Perempuan'
         ];
-
-        echo "Nama : {$Dosen ['nama']} <br>";
-        echo "Domisili : {$Dosen ['domisili']} <br>";
-        echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
-        
         ?>
+
+        <table>
+            <tr>
+                <th>Label</th>
+                <th>Data</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+            </tr>
+        </table>
     </body>
-    </html>
+</html>
