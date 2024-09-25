98fc4274aa3add6e310462b0bb19dbc0f77bfa97
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2024-09-25 13:50:34 +0700

    js5-praktikum1

---
+++ JOBSHEET5/array_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Array Terindeks</h2>
+<?php
+    $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    echo $Listdosen[2] . "<br>";
+    echo $Listdosen[0] . "<br>";
+    echo $Listdosen[1] . "<br>";
+
+    echo "<br>";
+    foreach ($Listdosen as $Dosen) {
+        echo $Dosen, "<br>";
+    }
+?>
+</body>
+</html>
