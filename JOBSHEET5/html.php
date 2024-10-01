789064c4a4b167d511008f4e216a4f12797a52ff
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2024-10-01 21:52:53 +0700

    js6-praktikum1

--- JOBSHEET5/html.php
+++ JOBSHEET5/html.php
@@ -1,8 +1,8 @@
 <?php
 echo '<html>';
-echo '<head><title>Cara02</ttile></head>';
+echo '<head><title>Cara02</title></head>';
 echo '<body>';
-echo '<p>Tanggal Hari ini : '.date('d M Y').'<./p>';
+echo '<p>Tanggal Hari ini : '.date('d M Y').'</p>';
 echo '</body>';
 echo '</html>';
 ?>
