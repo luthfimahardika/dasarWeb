09be3df2d88d5c3e5f1d954ca794d1044ad9ee41
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2024-10-09 13:32:56 +0700

    js7-praktikum3

--- JOBSHEET7/proses_form.php
+++ JOBSHEET7/proses_form.php
@@ -1,9 +1,27 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama = $_POST["nama"];
-    $email = $_POST["email"];
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Input PHP</title>
+    </head>
+    <body>
+        <h2>Form Input PHP</h2>
+        <form method="post" action="proses_form.php">
+            <label for="nama">Nama:</label>
+            <input type="text" name="nama" id="nama" required><br><br>
 
-    echo "Nama: " . $nama . "<br>";
-    echo "Email: " . $email;
-}
-?>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required><br><br>
+
+            <input type="submit" name="submit" id="submit" required><br><br>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nama = $_POST["nama"];
+        $email = $_POST["email"];
+
+        echo "Nama: " . $nama . "<br>";
+        echo "Email: " . $email;
+        }
+        ?>
+    </body>
+</html>
