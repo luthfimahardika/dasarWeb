69186e2f367f0581cc17c664a46f8f0741454fa4
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2024-11-06 17:06:26 +0700

    js11-praktikum5

--- JOBSHEET11/fungsi/edit.php
+++ JOBSHEET11/fungsi/edit.php
@@ -16,5 +16,52 @@ if (!empty($_SESSION['username'])) {
             pesan('danger', "Mengubah Jabatan Karena: " . mysqli_error($koneksi));
         }
         header("Location: ../index.php?page=jabatan");
+    
+    } elseif (!empty($_GET['anggota'])) {
+        // Menggunakan fungsi antiinjection untuk membersihkan input
+        $user_id = antiinjection($koneksi, $_POST['id']);
+        $nama = antiinjection($koneksi, $_POST['nama']);
+        $jabatan_id = antiinjection($koneksi, $_POST['jabatan']);
+        $jenis_kelamin = antiinjection($koneksi, $_POST['jenis_kelamin']);
+        $alamat = antiinjection($koneksi, $_POST['alamat']);
+        $no_telp = antiinjection($koneksi, $_POST['no_telp']);
+        $username = antiinjection($koneksi, $_POST['username']);
+        // Membuat query untuk mengupdate anggota
+        $query_anggota = "UPDATE anggota SET nama = '$nama', jenis_kelamin = '$jenis_kelamin', alamat = '$alamat', no_telp = '$no_telp', jabatan_id = '$jabatan_id' WHERE user_id = '$user_id'";
+        // Menjalankan query dan mengecek apakah berhasil
+        if (mysqli_query($koneksi, $query_anggota)) {
+            // Memeriksa apakah password juga perlu diubah
+            if (!empty($_POST['password'])) {
+                $password = $_POST['password'];
+                // Generate random salt
+                $salt = bin2hex(random_bytes(16));
+                // Gabungkan salt dengan password
+                $combined_password = $salt . $password;
+                // Hash password dengan salt
+                $hashed_password = password_hash($combined_password, PASSWORD_BCRYPT);
+                // Membuat query untuk mengupdate user
+                $query_user = "UPDATE user SET username = '$username', password = '$hashed_password', salt = '$salt' WHERE id = '$user_id'";
+                // Menjalankan query dan mengecek apakah berhasil
+                if (mysqli_query($koneksi, $query_user)) {
+                    pesan('success', "Anggota Telah Diubah.");
+                } else {
+                    pesan('warning', "Data Anggota Berhasil Diubah, Tetapi Password Gagal Diubah Karena: " . mysqli_error($koneksi));
+                }
+            } else {
+                // Jika tidak ada perubahan password, hanya update username
+                $query_user = "UPDATE user SET username = '$username' WHERE id = '$user_id'";
+                // Menjalankan query dan mengecek apakah berhasil
+                if (mysqli_query($koneksi, $query_user)) {
+                    pesan('success', "Anggota Telah Diubah.");
+                } else {
+                    pesan('warning', "Data Anggota Berhasil Diubah, Tetapi Username Gagal Diubah Karena: " . mysqli_error($koneksi));
+                }
+            }
+        } else {
+            pesan('danger', "Gagal Mengubah Anggota Karena: " . mysqli_error($koneksi));
+        }
+        // Mengalihkan kembali ke halaman anggota
+        header("Location: ../index.php?page=anggota");
+        exit(); // Menghentikan eksekusi skrip lebih lanjut
     }
 }
