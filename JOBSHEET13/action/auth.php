6cdecc62b6740b17a169cf67482647a81bcce4b9
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2024-11-25 16:29:30 +0700

    js13tambahan-praktikum3

--- JOBSHEET13/action/auth.php
+++ JOBSHEET13/action/auth.php
@@ -1,21 +1,20 @@
-<?php 
-include("../lib/Session.php");
-include("../lib/Connection.php");
+<?php
+include('../lib/Session.php');
+include('../lib/Connection.php');
 
 $session = new Session();
-$act = isset($_GET['act'])? strtolower($_GET['act']):'';
-if($act== 'login'){
+
+$act = isset($_GET['act']) ? strtolower($_GET['act']) : '';
+
+if ($act == 'login') {
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    include('../model/UserModel.php');
     // digunakan untuk query user
-    $query = $db->prepare('select * from m_user where username = ?');
-    $query->bind_param('s', $username);
-    $query->execute();
-
-    // untuk ambil data
-    $data = $query->get_result()->fetch_assoc();
-
+    $user = new UserModel();
+    $data = $user->getSingleDataByKeyword('username', $username);
     // jika password sesuai
     if (password_verify($password, $data['password'])) {
         $session->set('is_login', true);
@@ -23,16 +22,14 @@ if($act== 'login'){
         $session->set('name', $data['nama']);
         $session->set('level', $data['level']);
         $session->commit();
-
-        header('location: ../index.php', false);
+        header('Location: ../index.php', false);
     } else {
         $session->setFlash('status', false);
         $session->setFlash('message', 'Username dan password salah.');
         $session->commit();
         header('Location: ../login.php', false);
     }
-} elseif ($act === 'logout') {
+} else if ($act == 'logout') {
     $session->deleteAll();
     header('Location: ../login.php', false);
 }
-?>
